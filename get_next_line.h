1b6e52beb8265ff49132235e37aa10d18668577c
Yuer Liu <yuerliu@1-C-26.42heilbronn.de>
2024-12-10 19:23:38 +0100

    added strlcpy to header

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: yuerliu <yuerliu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 21:50:55 by yuerliu           #+#    #+#             */
-/*   Updated: 2024/12/08 18:23:35 by yuerliu          ###   ########.fr       */
+/*   Updated: 2024/12/10 19:23:18 by yuerliu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,13 @@
 # include <unistd.h>
 
 size_t		ft_strlen(char const *str);
-char		*ft_strdup(const char *s1);
-char		*ft_strjoin(char const *s1, char const *s2);
-char		*get_next_line(int fd);
-char		*get_da_file(int fd, char *rdin);
-char		*ft_strchr(char *s, int c);
-char		*ft_substr(char const *s, unsigned int start, size_t len);
-void		*shorty(void *dst, const void *src, size_t len);
+size_t		ft_strlcpy(char *dest, const char *src, size_t size);
+char			*ft_strdup(const char *s1);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*get_next_line(int fd);
+char			*get_da_file(int fd, char *rdin);
+char			*ft_strchr(char *s, int c);
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+void			*shorty(void *dst, const void *src, size_t len);
 
 #endif
